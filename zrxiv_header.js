bdd44fbb5133a9e3481abb705aa9738fcc5c650c
Vadim Kantorov <vadimkantorov@gmail.com>
2018-11-03 14:29:42 +0100

    ...

--- zrxiv_header.js
+++ zrxiv_header.js
@@ -3,14 +3,26 @@ function async_chrome_storage_sync_get(obj)
 	return new Promise(resolve => chrome.storage.sync.get(obj, resolve));
 }
 
+function delay(seconds)
+{
+	return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
 class ZrxivGithubBackend
 {
 	constructor(zrxiv_github_repo, zrxiv_github_token, href)
 	{
 		const [match, username, repo] = new RegExp('([^/]+)\.github.io/(.+)', 'g').exec(zrxiv_github_repo);
-		this.doc_id = new RegExp('abs/(\\d+\.\\d+)', 'g').exec(href)[1];
 		this.api = 'https://api.github.com/repos/' + username + '/' + repo;
 		this.auth_token = username + ':' + zrxiv_github_token;
+		this.doc = null;
+		this.sha = null;
+		this.retry_delay_seconds = 2;
+	}
+
+	github_api_request(relative_url, method, body)
+	{
+		return fetch(this.api + relative_url, Object.assign({method : method || 'get', headers : Object.assign({Authorization : 'Basic ' + btoa(this.auth_token)}, body != null {'Content-Type' : 'application/json'} : {})}, body != null ? {body : JSON.stringify(body)} : {}));
 	}
 
 	async parse_arxiv_document()
@@ -19,103 +31,100 @@ class ZrxivGithubBackend
 		const entry = document.createRange().createContextualFragment(xml).querySelector('entry');
 		return {
 			title : entry.querySelector('title').innerText, 
-			url : window.location.href,
 			author : Array.from(entry.querySelectorAll('author name')).map(elem => elem.innerText),
-			abstract : entry.querySelector('summary').innerText
+			abstract : entry.querySelector('summary').innerText,
+			id : new RegExp('abs/(\\d+\.\\d+)', 'g').exec(entry.querySelector('id').innerText)[1],
+			date : Math.floor(new Date().getTime() / 1000),
+			url : window.location.href,
+			tags : []
 		};
 	}
 
-	get_doc()
+	async init_doc()
 	{
-		return fetch(this.api + '/contents/_data/documents/' + this.doc_id + '.json', 
+		this.doc = await this.parse_arxiv_document();
+		const resp = await this.github_api_request('/contents/_data/documents/' + this.doc.id + '.json');
+		if(resp.status == 200)
 		{
-			headers : {'Authorization' : 'Basic ' + btoa(this.auth_token)}
-		});
+			const {content, sha} = await resp.json();
+			this.doc = JSON.parse(atob(content));
+			this.sha = sha;
+		}
 	}
 
 	get_tags()
 	{
-		return fetch(this.api + '/contents/_data/tags',
-		{
-			headers : {'Authorization' : 'Basic ' + btoa(this.auth_token)}
-		});
+		return this.github_api_request('/contents/_data/tags');
 	}
 
-	put_doc(message, doc, sha)
+	put_doc(message, sha, retry)
 	{
-		return fetch(this.api + '/contents/_data/documents/' + this.doc_id + '.json',
+		return this.github_api_request('/contents/_data/documents/' + this.doc.id + '.json', 'put', Object.assign({message : message + this.doc.id, content : btoa(JSON.stringify(this.doc, null, 2))}, sha ? {sha : sha} : {}))
+		.catch(async resp => 
 		{
-			method : 'put',
-			headers : 
+			if(resp.status == 409 && retry != false)
 			{
-				'Content-Type' : 'application/json',
-				'Authorization' : 'Basic ' + btoa(this.auth_token)
-			},
-			body : JSON.stringify(Object.assign({message : message + this.doc_id, content : btoa(JSON.stringify(doc, null, 2))}, sha ? {sha : sha} : {}))
+				await delay(this.retry_delay_seconds);
+				return this.put_doc(message, sha ? ((await this.init_doc()) || this.sha) : null, false);
+			}
 		})
 	}
 
-	async del_doc()
+	async del_doc(retry)
 	{
-		let response = await this.get_doc();
-		let json = await response.json();
-		return fetch(this.api + '/contents/_data/documents/' + this.doc_id + '.json',
+		return this.github_api_request('/contents/_data/documents/' + this.doc.id + '.json', 'delete', {message : 'Delete ' + this.doc.id, sha : this.sha})
+		.catch(async resp => 
 		{
-			method : 'delete',
-			headers : 
+			if(resp.status == 409 && retry != false)
 			{
-				'Content-Type' : 'application/json',
-				'Authorization' : 'Basic ' + btoa(this.auth_token)
-			},
-			body : JSON.stringify({message : 'Delete ' + this.doc_id, sha : json.sha})
-		});
+				await delay(this.retry_delay_seconds);
+				await this.init_doc();
+				return this.del_doc(false);
+			}
+		})
 	}
 
-	add_tag(tag)
+	add_tag(tag, retry)
 	{
-		return fetch(this.api + '/contents/_data/tags/' + tag + '.json',
+		return this.github_api_request('/contents/_data/tags/' + tag + '.json', 'put', {message : 'Create tag ' + tag, content : '{}' })
+		.catch(async resp => 
 		{
-			method : 'put',
-			headers :
+			if(resp.status == 409 && retry != false)
 			{
-				'Content-Type' : 'application/json',
-				'Authorization' : 'Basic ' + btoa(this.auth_token)
-			},
-			body : JSON.stringify({message : 'Create tag ' + tag, content : '{}' })
+				await delay(this.retry_delay_seconds);
+				return this.add_tag(tag, false);
+			}
 		});
 	}
 
 	add_doc()
 	{
-		const doc = {id : this.doc_id, title : document.title, url : window.location.href, date : Math.floor(new Date().getTime() / 1000), tags : [] };
-		return this.put_doc('Add ', doc);
+		return this.put_doc('Add ');
 	}
 
 	async toggle_tag(tag, checked)
 	{
-		const json = await (await this.get_doc()).json();
-		const doc = JSON.parse(atob(json.content));
-		const checked_old = doc.tags.indexOf(tag) >= 0;
-		if(checked && !checked_old)
-			doc.tags.push(tag);
-		else if(!checked && checked_old)
-			doc.tags = doc.tags.filter(x => x != tag);
-		return this.put_doc('Change tag of ', doc, json.sha);
+		const idx_old = this.doc.tags.indexOf(tag);
+		if(checked && idx_old < 0)
+			this.doc.tags.push(tag);
+		else if(!checked && idx_old >= 0)
+			this.doc.tags.splice(idx_old, 1);
+		return this.put_doc('Change tag of ', this.sha);
 	}
 
 	async auto_save(action)
 	{
-		let prevent_auto_save = (await async_chrome_storage_sync_get({prevent_auto_save : []})).prevent_auto_save;
+		let prevent_auto_save = (await async_chrome_storage_sync_get({prevent_auto_save : {}})).prevent_auto_save;
 		if(action == null)
-			return prevent_auto_save.indexOf(this.doc_id) >= 0;
-		else if(action == false && prevent_auto_save.indexOf(this.doc_id) < 0)
+			return prevent_auto_save[this.doc.id];
+		else if(action == false)
 		{
-			prevent_auto_save.push(this.doc_id);
+			prevent_auto_save[this.doc.id] = true;
 			chrome.storage.sync.set({prevent_auto_save : prevent_auto_save});
 		}
-		else if(action == true && prevent_auto_save.indexOf(this.doc_id) >= 0)
+		else if(action == true && prevent_auto_save.indexOf(this.doc.id) >= 0)
 		{
-			prevent_auto_save = prevent_auto_save.filter(x => x != this.doc_id);
+			delete prevent_auto_save[this.doc.id];
 			chrome.storage.sync.set({prevent_auto_save : prevent_auto_save});
 		}
 	}
@@ -123,7 +132,7 @@ class ZrxivGithubBackend
 
 function zrxiv_make_checkbox(zrxiv_api, tag, checked)
 {
-	let label = document.importNode(document.getElementById('zrxiv_checkbox').content.firstChild, true);
+	let label = document.importNode(document.getElementById('zrxiv_checkbox').content, true).firstChild;
 	let checkbox = label.firstChild;
 	label.appendChild(document.createTextNode(tag));
 	checkbox.value = tag;
@@ -217,17 +226,16 @@ async function zrxiv_init(options)
 	document.getElementById('zrxiv_toggle').addEventListener('click', function(event) { zrxiv_toggle(zrxiv_api, this.dataset.action); } );
 	document.getElementById('zrxiv_tag').addEventListener('keyup', function(event) { if (event.keyCode == 13) document.getElementById('zrxiv_tag_add').click(); });
 
-	let [doc, tags] = await Promise.all([zrxiv_api.get_doc(), zrxiv_api.get_tags()]);
-	[doc, tags] = await Promise.all([doc.status == 200 ? await doc.json() : null, tags.status == 200 ? await tags.json() : []]);
-	zrxiv_tags_render(zrxiv_api, doc != null, doc != null ? JSON.parse(atob(doc.content)).tags : [], tags.map(x => x.name.split('.').slice(0, -1).join('.')));
-	if(doc == null)
+	let [doc, tags] = await Promise.all([zrxiv_api.init_doc(), zrxiv_api.get_tags()]);
+	zrxiv_tags_render(zrxiv_api, zrxiv_api.sha != null, zrxiv_api.doc.tags, (tags.status == 200 ? await tags.json() : []).map(x => x.name.split('.').slice(0, -1).join('.')));
+	if(zrxiv_api.sha == null)
 	{
 		if(!options.zrxiv_auto_save_timeout || await zrxiv_api.auto_save())
 			zrxiv_toggle('prevent-auto-save');
 		else
 		{
 			zrxiv_toggle(zrxiv_api, 'auto-save', options.zrxiv_auto_save_timeout);
-			await new Promise(resolve => setTimeout(resolve, options.zrxiv_auto_save_timeout * 1000));
+			await delay(options.zrxiv_auto_save_timeout);
 			zrxiv_toggle(zrxiv_api, 'save');
 		}
 	}
