a0246db694c9ee493d74b6929e358c262edfbfca
Vadim Kantorov <vadimkantorov@gmail.com>
2018-11-08 18:37:14 +0100

    ...

--- zrxiv_background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-	chrome.storage.sync.get({zrxiv_github_repo: chrome.runtime.getManifest().homepage_url}, function(options)
-	{
-		const url = options.zrxiv_github_repo.startsWith('http') ? options.zrxiv_github_repo : 'https://' + options.zrxiv_github_repo;
-		chrome.tabs.create({ url: url });
-	});
-});
