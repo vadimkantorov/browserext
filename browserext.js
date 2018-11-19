ac8ba95d35e3ba30654dee4974c019bcff08a7f3
Vadim Kantorov <vadimkantorov@gmail.com>
2018-11-19 13:01:44 +0100

    ...

--- browserext.js
+++ browserext.js
@@ -1,6 +1,5 @@
-if(chrome && typeof(browser) == 'undefined')
+if(typeof(chrome) != 'undefined' && typeof(browser) == 'undefined')
 {
 	browser = chrome;
-	let chrome_storage_sync_get = chrome.storage.sync.get;
-	browser.storage.sync.get = keys => new Promise(resolve => chrome_storage_sync_get(keys, resolve));
+	browser.storage.sync.get = (keys, chrome_storage_sync_get = chrome.storage.sync.get) => new Promise(resolve => chrome_storage_sync_get(keys, resolve));
 }
