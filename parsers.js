542fc7e63a1b4e7f4a543265f772e9536d6a9d97
Vadim Kantorov <vadimkantorov@gmail.com>
2018-12-19 03:53:30 +0100

    ...

--- parsers.js
+++ parsers.js
@@ -2,12 +2,13 @@ async function arxiv(page, href, date)
 {
 	const entry = page.createRange().createContextualFragment(await (await fetch(href.replace('arxiv.org/abs/', 'export.arxiv.org/api/query?id_list='))).text()).querySelector('entry');
 	const pdf = find_meta(page, 'citation_pdf_url');
+	const url = pdf.replace('/pdf/', '/abs/');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : decomma_authors(find_meta(page, 'citation_author', Array)),
 		abstract : entry.querySelector('summary').innerText,
 		id : 'arxiv.' + find_meta(page, 'citation_arxiv_id').replace('/', '_'),
-		url : pdf.replace('/pdf/', '/abs/'),
+		url : url,
 		pdf : pdf,
 		source : 'arxiv.org',
 		date : date,
@@ -18,14 +19,15 @@ async function arxiv(page, href, date)
 async function nips(page, href, date)
 {
 	const pdf = find_meta(page, 'citation_pdf_url');
+	const url = pdf.replace('.pdf', '.html');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : page.querySelector('.abstract').innerText,
 		id : 'neurips.' + new RegExp('/paper/(\\d+)-.+').exec(pdf)[1],
-		url : href,
+		url : url,
 		pdf : pdf,
-		bibtex : format_bibtex(await (await fetch(find_link_by_text(page, '[BibTeX]').replace('http://', 'https://'))).text()),
+		bibtex : format_bibtex(await (await fetch(find_link_by_text(page, '[BibTeX]').replace('http://', 'https://'))).text(), url, pdf),
 		source : 'nips.cc',
 		date : date,
 		tags : []
@@ -35,14 +37,16 @@ async function nips(page, href, date)
 async function openreview(page, href, date)
 {
 	const entry = (await (await fetch(href.replace('/forum?id=', '/notes?forum='))).json()).notes.filter(note => note.original != null)[0];
+	const pdf = find_meta(page, 'citation_pdf_url');
+	const url = pdf.replace('/pdf', '/forum');
 	return {
 		title : entry.content.title,
 		authors : entry.content.authors,
 		abstract : entry.content.abstract,
 		id : 'openreview.' + entry.id,
-		url : href,
-		pdf : 'https://openreview.net' + entry.content.pdf,
-		bibtex : format_bibtex(entry.content._bibtex),
+		url : url,
+		pdf : pdf,
+		bibtex : format_bibtex(entry.content._bibtex, url, pdf),
 		source : 'openreview.net',
 		date : date,
 		tags : []
@@ -51,15 +55,16 @@ async function openreview(page, href, date)
 
 async function cvf(page, href, date)
 {
-	const pdf = find_meta(page, 'citation_pdf_url');
+	const pdf = find_meta(page, 'citation_pdf_url').replace('openaccess/', '');
+	const url = pdf.replace('.pdf', '.html');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : decomma_authors(find_meta(page, 'citation_author', Array)),
 		abstract : page.querySelector('#abstract').innerText,
 		id : 'cvf.' + pdf.split('/').pop().replace('_paper.pdf', ''),
-		url : href,
+		url : url,
 		pdf : pdf,
-		bibtex : format_bibtex(page.querySelector('.bibref').innerText),
+		bibtex : format_bibtex(page.querySelector('.bibref').innerText, url, pdf),
 		source : 'thecvf.com',
 		date : date,
 		tags : [],
@@ -71,14 +76,16 @@ async function cvf(page, href, date)
 async function hal(page, href, date)
 {
 	const entry = (await (await fetch(href + (href.endsWith('/') ? '' : '/') + 'json')).json()).response.docs[0];
+	const url = entry.uri_s;
+	const pdf = entry.files_s[0];
 	return {
 		title : entry.title_s[0],
 		authors : entry.authFullName_s,
 		abstract : entry.abstract_s[0],
 		id : 'hal.' + entry.halId_s.replace('hal-', ''),
-		url : entry.uri_s,
-		pdf : entry.files_s[0],
-		bibtex : format_bibtex(entry.label_bibtex),
+		url : url,
+		pdf : pdf,
+		bibtex : format_bibtex(entry.label_bibtex, url, pdf),
 		source : 'hal.archives-ouvertes.fr',
 		date : date,
 		tags : []
@@ -97,15 +104,17 @@ function pnas(page, href, date)
 
 async function ssrn(page, href, date)
 {
+	const url = find_meta(page, 'citation_abstract_html_url');
+	const pdf = find_meta(page, 'citation_pdf_url');
 	const doi = find_meta(page, 'citation_doi');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : decomma_authors(find_meta(page, 'citation_author', Array)),
 		abstract : page.querySelector('.abstract-text>p').innerText,
 		id : doi.split('/')[1],
-		url : find_meta(page, 'citation_abstract_html_url'),
-		pdf : find_meta(page, 'citation_pdf_url'),
-		bibtex : format_bibtex(await bibtex_crossref(doi)),
+		url : url,
+		pdf : pdf,
+		bibtex : format_bibtex(await bibtex_crossref(doi), url, pdf),
 		source : 'ssrn.com',
 		date : date,
 		tags : [],
@@ -116,14 +125,15 @@ async function ssrn(page, href, date)
 async function projecteuclid(page, href, date)
 {
 	const url = find_meta(page, 'citation_abstract_html_url');
+	const pdf = find_meta(page, 'citation_pdf_url');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : page.querySelector('.abstract-text>p').innerText,
 		id : url.replace('https://projecteuclid.org/', '').replace('/', '_'),
 		url : url,
-		pdf : find_meta(page, 'citation_pdf_url'),
-		bibtex : format_bibtex(await (await fetch(url.replace('.org/', '.org/export_citations?format=bibtex&h='))).text()),
+		pdf : pdf,
+		bibtex : format_bibtex(await (await fetch(url.replace('.org/', '.org/export_citations?format=bibtex&h='))).text(), url, pdf),
 		source: 'projecteuclid.org',
 		date : date,
 		tags : [],
@@ -133,15 +143,17 @@ async function projecteuclid(page, href, date)
 
 async function aps(page, href, date)
 {
+	const url = page.querySelector('link[rel="canonical"]').href;
+	const pdf = find_meta(page, 'citation_pdf_url');
 	const doi = find_meta(page, 'citation_doi');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : page.querySelector('.abstract .content >p').innerText,
 		id : 'aps.' + strip_doi_part(doi),
-		url : page.querySelector('link[rel="canonical"]').href,
-		pdf : find_meta(page, 'citation_pdf_url'),
-		bibtex : await (await fetch(page.querySelector('meta[property="og:url"]').content.replace('abstract', 'export'))).text(),
+		url : url,
+		pdf : pdf,
+		bibtex : format_bibtex(await (await fetch(page.querySelector('meta[property="og:url"]').content.replace('abstract', 'export'))).text(), url, pdf),
 		source : 'aps.org',
 		date : date,
 		tags : [],
@@ -152,14 +164,15 @@ async function aps(page, href, date)
 function pmlr(page, href, date)
 {
 	const url = find_meta(page, 'citation_abstract_html_url');
+	const pdf = find_meta(page, 'citation_pdf_url');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : page.querySelector('.abstract').innerText,
 		id : 'mlr.' + new RegExp(/.+\/(v\d+)\/(.+).html/, 'g').exec(url).slice(1).join('.'),
 		url : url,
-		pdf : find_meta(page, 'citation_pdf_url'),
-		bibtex : format_bibtex(page.querySelector('#bibtex').innerText),
+		pdf : pdf,
+		bibtex : format_bibtex(page.querySelector('#bibtex').innerText, url, pdf),
 		source : 'mlr.press',
 		date : date,
 		tags: [],
@@ -169,14 +182,15 @@ function pmlr(page, href, date)
 async function jmlr(page, href, date)
 {
 	const pdf = find_meta(page, 'citation_pdf_url');
+	const url = pdf.replace('.pdf', '.html');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : page.querySelector('#content>h3').nextSibling.textContent.trim(),
 		id : 'jmlr.' + new RegExp(/.+\/papers\/(.+)\/(.+)\/.+/, 'g').exec(pdf).slice(1).join('.'),
-		url : pdf.replace('.pdf', '.html'),
+		url : url,
 		pdf : pdf,
-		bibtex : await (await fetch(find_link_by_text(page, 'bib'))).text(),
+		bibtex : format_bibtex(await (await fetch(find_link_by_text(page, 'bib'))).text(), url, pdf),
 		source : 'jmlr.org',
 		date : date,
 		tags : []
@@ -193,17 +207,17 @@ async function iacr(page, href, date)
 	{
 		if(p[i].innerText.includes('Category'))
 			break;
-
 		abstract += p[i].lastChild.textContent + ' ';
 	}
+	const pdf = a[0].href;
 	return {
 		title : page.querySelector('b').innerText,
 		authors : page.querySelector('i').innerText.split(' and '),
 		abstract : abstract.trim(),
 		id : 'iacr.' + url.innerText.replace('.', '').replace(/\//g, '.'),
 		url : url.href,
-		pdf : a[0].href,
-		bibtex : page.createRange().createContextualFragment(await (await fetch(a[1].href)).text()).querySelector('pre').innerText,
+		pdf : pdf,
+		bibtex : format_bibtex(page.createRange().createContextualFragment(await (await fetch(a[1].href)).text()).querySelector('pre').innerText, url.href, pdf),
 		source : 'iacr.org',
 		date : date,
 		tags : []
@@ -212,14 +226,16 @@ async function iacr(page, href, date)
 
 async function highwire(page, href, date, provider, source)
 {
+	const url = find_meta(page, 'citation_public_url');
+	const pdf = find_meta(page, 'citation_pdf_url');
 	return {
 		title : find_meta(page, 'citation_title'),
 		authors : find_meta(page, 'citation_author', Array),
 		abstract : find_meta(page, 'citation_abstract'),
 		id : provider + '.' + strip_version(find_meta(page, 'citation_id')),
-		url : find_meta(page, 'citation_public_url'),
-		pdf : find_meta(page, 'citation_pdf_url'),
-		bibtex : format_bibtex(await (await fetch(find_link_by_text(page, 'BibTeX'))).text()),
+		url : url,
+		pdf : pdf,
+		bibtex : format_bibtex(await (await fetch(find_link_by_text(page, 'BibTeX'))).text(), url, pdf),
 		source : source,
 		date : date,
 		tags : [],
@@ -258,17 +274,65 @@ function decomma_authors(authors)
 	return authors.map(a => a.split(', ').reverse().join(' '));
 }
 
-function format_bibtex(bibtex)
+function parse_bibtex(text)
 {
-	/*const entry_type_key = new RegExp(/\s*(@.+)\s*{\s*([^,]+)\s*,/, 'g');
-	const field = new RegExp(/(.+)/, 'g');
-	const [entry_type, key] = entry_type_key.exec(bibtex).slice(1);
+	const parse_bibtex_line = function(text)
+	{
+		let m = text.match(/^\s*(\S+?)\s*=\s*/);
+		if (!m) 
+			throw new Error('Unrecogonised line format');
+		const name = m[1];
+		const search = text.slice(m[0].length);
+		const re = /[\n\r,{}]/g;
+		const length = m[0].length;
+		let braceCount = 0;
+		do
+		{
+			m = re.exec(search);
+			if (m[0] === '{')
+				braceCount++;
+			else if (m[0] === '}')
+			{
+				if (braceCount ===  0)
+					throw new Error('Unexpected closing brace: "}"');
+				braceCount--;
+			}
+		}
+		while (braceCount > 0);
+		return [name, search.slice(0, re.lastIndex), length + re.lastIndex + m[0].length];
+	};
 
-	bibtex = bibtex.replace(entry_type_key, '{')
-	bibtex = bibtex.replace(new RegExp(/\b(.+)\s*=/, 'g'), '$1 :');*/
-	// https://stackoverflow.com/questions/34221996/how-to-parse-complex-bibtex-items-with-javascript-and-regex
-	
-	return bibtex.replace(/\n\n/g, '\n').replace(/    \n/g, '\n').replace(/    /g, ' ').replace(/\t/g, ' ').replace(/\n/g, '\n  ').replace(/\n  }/g, '\n}').replace('@InProceedings', '@inproceedings').trim();
+	const m = text.match(/^\s*@([^{]+){\s*([^,\n]+)[,\n]/);
+	if (!m) 
+		throw new Error('Unrecogonised header format');
+	text = text.slice(m[0].length).trim();
+	let bib = {};
+	while (text[0] !== '}')
+	{
+		const [field, value, length] = parse_bibtex_line(text);
+		bib[field.toLowerCase()] = value;
+		text = text.slice(length).trim();
+	}
+	return [m[1].trim().toLowerCase(), m[2].trim().toLowerCase(), bib];
+}
+
+function format_bibtex(bibtex, url, pdf)
+{
+	try
+	{
+		let [record_type, citation_key, bib] = parse_bibtex(bibtex);
+		bib.url = bib.url || `{${url}}`;
+		bib.pdf = bib.pdf || `{${pdf}}`;
+		delete bib.abstract;
+		const header = ['title', 'author', 'booktitle', 'journal', 'year', 'doi'], footer = ['note', 'pdf', 'url'];
+		const keys = header.filter(k => bib.hasOwnProperty(k)).concat(Object.keys(bib).sort().filter(k => !header.includes(k) && !footer.includes(k))).concat(footer.filter(k => bib.hasOwnProperty(k)));
+		return `@${record_type}{${citation_key},\n` + keys.map(k => `  ${k} = ${bib[k]}`).join(',\n') + '\n}';
+	}
+	catch(ex)
+	{
+		console.log('bibtex parsing error', ex.message);
+		return bibtex;
+	}
 }
 
 function parse_doc(page, href, date)
